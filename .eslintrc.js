2c1d0f66ea86d0daa44e3d334655115e768c7578
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-07-25 18:55:01 +0500

    Add eslint packages

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    webextensions: true,
+  },
+  globals: {
+    chrome: true,
+  },
+  extends: [
+    'plugin:flowtype/recommended',
+    'eslint:recommended',
+    'airbnb',
+  ],
+  plugins: [
+    'flowtype',
+  ],
+  rules: {
+    'arrow-parens': ['error', 'always'], // (arg) => thingy
+    'padded-blocks': 'off', // (arg) => {\n\n_body_\n\n}
+    'no-plusplus': 'off',
+    'consistent-return': 'off',
+  },
+};
