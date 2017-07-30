658c7f9db4123bc9dcfa81cd686afb7900816267
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-07-30 19:17:52 +0500

    Add install to index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import Utils from './utils';
 import ErrorCatchers from './error-catchers';
 import GetNotifiersSingleton from './get-notifiers-singleton';
 
+const Install = (configs) => {
+
+  const { handleErrorMessage } = GetNotifiersSingleton(configs);
+  ErrorCatchers.installListenersOn({ handleErrorMessage });
+
+}
+
 export {
   Utils,
   ErrorCatchers,
   GetNotifiersSingleton,
+  Install,
 };
