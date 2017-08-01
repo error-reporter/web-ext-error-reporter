ce049f0dc13a23f95a37b145a6857302033cfa1f
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-08-01 19:21:47 +0500

    Fix popup example

--- examples/src/pages/popup/index.js
+++ examples/src/pages/popup/index.js
@@ -3,21 +3,13 @@
 chrome.runtime.getBackgroundPage((bgWindow) =>
   bgWindow.Weer.ErrorCatchers.installListenersOn({ hostWindow: window, nameForDebug: 'PUP' }, () => {
 
-    //console.log('WW3?', bgWindow === window);
-    //throw new Error('BOOPUP');
     document.getElementById('btn').onclick = () => {
 
-      throw new Error('FIRE!');
+      throw new Error('ONCLK!');
 
     };
 
-    //throw new Error('PUPERR');
-
-    chrome.tabs.getCurrent(() => {
-
-      throw new Error('Chrome API callback (not caught by Weer)');
-
-    });
+    throw new Error('PUPERR');
 
   })
 );
