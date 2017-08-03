6a1c7e3f6904255b75d8c40c0198be97a09593bd
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-08-03 16:29:59 +0500

    Test-cover all Utils API

--- test/unit/get-notifiers-singleton/global-exports.js
+++ test/unit/get-notifiers-singleton/global-exports.js
@@ -5,6 +5,7 @@ describe('Weer', () => {
   it('exports global Weer variable', () => {
     assert.isDefined(Weer, 'Weer has been defined');
     assert.isDefined(Weer.Utils, 'Weer.Utils has been defined');
+    assert.isDefined(Weer.GetNotifiersSingleton, 'Weer.GetNotifiersSingleton has been defined');
   });
 
 });
