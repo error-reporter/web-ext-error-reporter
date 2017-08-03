c0f2222ea9b00423dca315351f94f54c4de87cbb
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-08-03 16:33:04 +0500

    Rephrase assertions in present tense

--- test/unit/get-notifiers-singleton/global-exports.js
+++ test/unit/get-notifiers-singleton/global-exports.js
@@ -1,11 +1,11 @@
 'use strict';
 
-describe('Weer', () => {
+describe('Weer.GetNotifiersSingleton', () => {
 
   it('exports global Weer variable', () => {
-    assert.isDefined(Weer, 'Weer has been defined');
-    assert.isDefined(Weer.Utils, 'Weer.Utils has been defined');
-    assert.isDefined(Weer.GetNotifiersSingleton, 'Weer.GetNotifiersSingleton has been defined');
+    assert.isDefined(Weer, 'Weer is defined');
+    assert.isDefined(Weer.Utils, 'Weer.Utils is defined');
+    assert.isDefined(Weer.GetNotifiersSingleton, 'Weer.GetNotifiersSingleton is defined');
   });
 
 });
