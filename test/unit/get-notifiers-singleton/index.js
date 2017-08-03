4853e437af21c49e2da27e6063bcf40e11e6ed41
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-08-03 20:46:26 +0500

    Test-cover getNotifiersSingleton API

---
+++ test/unit/get-notifiers-singleton/index.js
@@ -0,0 +1,90 @@
+'use strict';
+
+describe('Weer.GetNotifiersSingleton', () => {
+
+  it('exports global Weer variable', () => {
+    assert.isDefined(Weer, 'Weer is defined');
+    assert.isDefined(Weer.Utils, 'Weer.Utils is defined');
+    assert.isDefined(Weer.GetNotifiersSingleton, 'Weer.GetNotifiersSingleton is defined');
+  });
+
+  describe('GetNotifiersSingleton', () => {
+
+    const getNoty = Weer.GetNotifiersSingleton;
+    const noty = getNoty();
+
+    chrome.notifications.create.callsFake((id, ops, cb) => cb());
+
+    it('returns singleton', () => {
+
+      const one = getNoty();
+      const two = getNoty();
+      assert.isDefined(one);
+      expect(one).to.equal(two);
+
+    });
+
+    it('exposes `handleErrorMessage`', (done) => {
+
+      expect(noty.handleErrorMessage).to.throw(TypeError);
+      expect(chrome.notifications.create.callCount).to.not.be.ok;
+      noty.handleErrorMessage({ errorData: { message: 'Hi from error!', } }).then(
+        (ifNotied) => {
+
+          expect(ifNotied).to.be.true;
+          expect(chrome.notifications.create.callCount).to.equal(1);
+          chrome.notifications.create.reset();
+          done();
+
+        }
+      );
+
+    });
+
+    it('exposes `getErrorTypeToLabelMap`', () => {
+
+      assert.isDefined(noty.getErrorTypeToLabelMap);
+      const map = noty.getErrorTypeToLabelMap();
+      assert.instanceOf(map, Map);
+      expect(map.size).to.be.ok;
+      expect([...map.keys()]).to.include.members(['ext-error', 'pac-error']);
+
+    });
+
+    const map = noty.getErrorTypeToLabelMap();
+    const errTypes = [...map.keys()];
+
+    it('exposes `isOn`', () => {
+
+      const ifAllOn = errTypes.every((k) => noty.isOn(k));
+      assert(ifAllOn, 'All error types are "on" by default.');
+      expect(noty.isOn).to.throw(TypeError);
+      const errType = 'ext-error';
+      expect(noty.isOn(errType)).to.equal(true);
+      expect(() => noty.isOn('unknown-error-type')).to.throw(Error);
+
+    });
+
+    it('exposes `switch`', () => {
+
+      const errType = 'ext-error';
+      expect(noty.isOn(errType)).to.equal(true);
+      expect(() => noty.switch()).to.throw(Error);
+      expect(() => noty.switch('FOO')).to.throw(Error);
+      expect(noty.switch('off', errType)).to.be.undefined;
+      expect(noty.isOn(errType)).to.be.false;
+      noty.switch('off');
+      const ifAllOff = errTypes.every((k) => noty.isOn(k) === false);
+      expect(ifAllOff).to.be.true;
+      noty.switch('on');
+      const ifAllOn = errTypes.every((k) => noty.isOn(k));
+      expect(ifAllOn).to.be.true;
+      noty.switch('off', errType);
+      const ifAllStillOn = errTypes.every((k) => noty.isOn(k));
+      expect(ifAllStillOn).to.be.false;
+
+    });
+
+  });
+
+});
