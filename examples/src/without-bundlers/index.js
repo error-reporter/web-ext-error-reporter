1d82620b8f285879bcd1bea4ef140af8c3e160bc
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-08-04 23:19:12 +0500

    Use weer from parent folder, not from npm

--- examples/src/without-bundlers/index.js
+++ examples/src/without-bundlers/index.js
@@ -2,12 +2,16 @@
 
 console.log('Extension started.');
 
-Weer.Install({
+Weer.install(
+/*
+{
   errorReportingUrl: 'https://example.com/foo?title={{message}}&json={{json}}',
   extErrorIconUrl: 'https://example.com/img/ext-error-128.png',
   pacErrorIconUrl: 'https://example.com/img/pac-error-128.png',
   maskIconUrl: 'https://example.com/img/mask-128.png',
-});
+}
+*/
+);
 
 window.bar = function foo() {
   throw new Error('Err in BG');
