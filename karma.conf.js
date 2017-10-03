b835e133c69242638b487607472d391a85ea7c49
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-10-03 16:10:18 +0500

    Pass more lints, make firefox headless

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,4 @@
-'use strict';
-
+'use strict'; // eslint-disable-line
 
 const testTarget = process.env.TEST_TARGET;
 const allowedTargets = [
@@ -11,7 +10,7 @@ const allowedTargets = [
 if (!allowedTargets.includes(testTarget)) {
   console.error(
     'Please, provide TEST_TARGET environment variable. Allowed targets:',
-    allowedTargets.join(', ') + '.',
+    `${allowedTargets.join(', ')}.`,
   );
   process.exit(1);
 }
@@ -21,7 +20,7 @@ const getUmd = (target) => `./dist/umd/${target}.js`;
 const deps = ['utils', 'index'].includes(testTarget)
   ? [/* No deps. */]
   : [
-      getUmd('utils'),
+    getUmd('utils'),
   ]
 ;
 
@@ -31,17 +30,16 @@ const targetedFiles = [
   `./test/unit/${testTarget}/**/*.js`,
 ];
 
-module.exports = function(config) {
+module.exports = (config) => {
+
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'chai'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -52,12 +50,10 @@ module.exports = function(config) {
       ...targetedFiles,
     ],
 
-
     // list of files to exclude
     exclude: [
-      '**/*.swp'
+      '**/*.swp',
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -76,32 +72,34 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
-
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
     // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    // possible values:
+    // config.LOG_DISABLE || config.LOG_ERROR ||
+    // config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
       'ChromeHeadless',
-      'Firefox',
-//      'ChromeCanary',
+      'FirefoxHeadless',
+      // 'ChromeCanary',
     ],
-
+    customLaunchers: {
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: ['-headless'],
+      },
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -109,6 +107,7 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+    concurrency: Infinity,
+  });
+
+};
