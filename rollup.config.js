a7a1ab107d162e650bd4986378c2a97f7425835e
ilyaigpetrov <ilyaigpetrov@gmail.com>
2018-09-07 19:02:00 +0500

    style(root): confrom to the style

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import Path from 'path';
 import shell from 'shelljs';
-const { LERNA_PACKAGE_NAME, LERNA_ROOT_PATH } = process.env;
+
+const { LERNA_ROOT_PATH } = process.env;
 const PACKAGE_ROOT_PATH = process.cwd();
 
 /*
@@ -9,12 +10,13 @@ console.log('LERNA ROOT PATH', LERNA_ROOT_PATH);
 console.log('LERNA PKG NAME', LERNA_PACKAGE_NAME);
 */
 
+// eslint-disable-next-line
 const PKG = require(Path.join(PACKAGE_ROOT_PATH, 'package.json'));
 
 /*
   @weer/utils -> utils.js
   @weer/commons/error-types -> error-types.js
-**/
+*/
 const pkgNameToFilename = (name) => {
 
   if (name.startsWith('@weer')) {
@@ -23,11 +25,11 @@ const pkgNameToFilename = (name) => {
       @weer/common -> common.js
       @weer/common/error-type -> error-type.js
       @weer/common/private/debug -> private/debug.js
-    **/
-    return name.replace(/^@weer\/(?:.+?\/)?(.+)$/, '$1') + '.js';
+    */
+    return `${name.replace(/^@weer\/(?:.+?\/)?(.+)$/, '$1')}.js`;
   }
   return name;
-}
+};
 
 const filename = pkgNameToFilename(PKG.name);
 const allInOnePath = Path.join(LERNA_ROOT_PATH, 'packages', 'weer-weer');
@@ -36,23 +38,19 @@ const output = (outputFilePath) => [
   {
     file: Path.join(allInOnePath, outputFilePath),
     format: 'esm',
-    paths: (pkgName) => {
-
-      console.log('PATH CHANGE FOR', pkgName);
-      return `./${pkgNameToFilename(pkgName)}`;
-    },
+    paths: (pkgName) => `./${pkgNameToFilename(pkgName)}`,
     banner: `// Generated from package ${PKG.name} v${PKG.version}`,
   },
 ];
 
-export default PKG.module ?
-[
-  {
-    input: PKG.module,
-    output: output(filename),
-  },
-] :
-shell.ls('*.js', '**/*.js').map((jsFilePath) => ({
-  input: Path.join(PACKAGE_ROOT_PATH, jsFilePath),
-  output: output(jsFilePath),
-}));
+export default PKG.module
+  ? [
+    {
+      input: PKG.module,
+      output: output(filename),
+    },
+  ]
+  : shell.ls('*.js', '**/*.js').map((jsFilePath) => ({
+    input: Path.join(PACKAGE_ROOT_PATH, jsFilePath),
+    output: output(jsFilePath),
+  }));
